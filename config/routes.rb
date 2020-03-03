efc6286b83a7c297aa1512a0046c614ef561468f
jun <mohusroom@gamil.com>
2020-03-04 01:38:11 +0900

    route hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get 'hello/index', to: 'hello#index'
+	get 'hello/view', to: 'hello#view'
 end
